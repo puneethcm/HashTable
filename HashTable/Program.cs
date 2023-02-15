f78786ae0041ad54fa18e5d501488091754563d9
puneethcm <puneethkumarcm8050@gmail.com>
2023-02-15 19:55:02 +0530

    Small Paragraph Frequency

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HashTable;
 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Hash Table");
+        string paragraph = "To be or not to be";
+        CountNumbOfOccurence(paragraph);
+        Console.ReadLine();
+    }
+    public static void CountNumbOfOccurence(string paragraph)
+    {
+        MyMapNode<string, int> hashTabe = new MyMapNode<string, int>(6);
+
+        string[] words = paragraph.Split(' ');
+
+        foreach (string word in words)
+        {
+            if (hashTabe.Exists(word.ToLower()))
+                hashTabe.Add(word.ToLower(), hashTabe.Get(word.ToLower()) + 1);
+            else
+                hashTabe.Add(word.ToLower(), 1); //to,1 
+        }
+        Console.WriteLine("Displaying after add operation");
+        hashTabe.Display();
+        
+    }
+}
